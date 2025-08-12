dac322c63bf9bbdb7de57c428f3a1228b25db7d1
ErickUEA <ea.pazc@uea.edu.ec>
2025-08-12 17:07:37 -0500

    tarea

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+ return "<h1>¡Hola, Mundo</h1>"
+
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+ return f"<h1>¡Hola, {nombre}!</h1>"
+
+if __name__ == '__main__':
+ app.run(debug=True)
